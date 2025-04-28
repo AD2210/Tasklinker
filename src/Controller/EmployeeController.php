03600b0b0ae73e61bc8b727e3fb830dde80ddb40
Anthony Delbee <anthony.delbee@gmail.com>
2025-04-28 09:51:09 +0200

    create page and route to show all employees, add template and method in controller to manage EmployeeType

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Employee;
+use App\Form\EmployeeType;
 use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EmployeeController extends AbstractController
 {
+    #[Route('/employee/{id}/edit', name: 'app_employee_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function employeeEdition(Employee $employee, Request $request, EntityManager $entityManager): Response
+    {
+        $form = $this->createForm(EmployeeType::class, $employee);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $entityManager->persist($employee);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_employee');
+        }
+
+        return $this->render('project/employeeForm.html.twig', [
+            'form' => $form,
+            'project' => $employee
+        ]);
+    }
+
     #[Route('/employee', name: 'app_employee')]
     public function index(EmployeeRepository $employeeRepository): Response
     {
         $employees = $employeeRepository->findAll();
 
-        return $this->render('employee/index.html.twig', [
+        return $this->render('employee/employees.html.twig', [
             'employees' => $employees
         ]);
     }
