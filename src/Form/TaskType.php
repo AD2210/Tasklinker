fe567a5122ba51aa62d34b91844743c863f63d5a
Anthony Delbee <anthony.delbee@gmail.com>
2025-04-30 11:24:59 +0200

    Clean unused requirement

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -3,14 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Employee;
-use App\Entity\Project;
 use App\Entity\Task;
-use Doctrine\Persistence\Proxy;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
