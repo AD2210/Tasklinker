8d918cc0cf45e3f0e2e528c640b7de1490a9302f
Anthony Delbee <anthony.delbee@gmail.com>
2025-05-04 11:29:03 +0200

    modifi condition to set status onlu when creating new task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,11 +6,14 @@ use App\Entity\Task;
 use App\Form\TaskType;
 use App\Entity\Project;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Null_;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use function PHPUnit\Framework\isNull;
 
 final class TaskController extends AbstractController
 {
@@ -37,8 +40,13 @@ final class TaskController extends AbstractController
     ): Response {
         
         $task ??= new Task; //Si aucune tâche passée = Nouvelle, si non edition
+        
+        // On set les valeurs par défaut dans le formulaire : project et status si nouvelle tâche
         $task->setProject($project);
-        $task->setStatus($task_status);
+        if ( !$task_status == null){
+           $task->setStatus($task_status); 
+        }
+
         $form = $this->createForm(TaskType::class, $task, [
             // on passe le project en parametre du formulaire pour selectionner uniquement les employés affecté au project
             'project' => $project,
